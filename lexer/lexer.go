f3be346adac8c4457f5c4369f3858aca3ed70aed
FT1006 <frankthp1234@gmail.com>
2025-04-06 11:14:59 +0100

    update (l *Lexer) NextToken(); add (l *Lexer) readIdentifier(), isLetter(), var keywords, LookupIdent()

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,10 +6,10 @@ import (
 
 // Lexer represents a lexical scanner.
 type Lexer struct {
-	input 		 string
-	position 	 int // current position in input (points to current char)
-	readPosition int // current reading position in input (after current char)
-	ch 			 byte // current char under examination
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
 }
 
 // New returns a new Lexer.
@@ -22,56 +22,86 @@ func New(input string) *Lexer {
 
 // readChar initializes the Lexer's current character.
 func (l *Lexer) readChar() {
-    if l.readPosition >= len(l.input) {
-        l.ch = 0 // indicates end of input
-    } else {
-        l.ch = l.input[l.readPosition]
-    }
-    l.position = l.readPosition
-    l.readPosition += 1
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // indicates end of input
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// peekChar returns the next character without advancing the read position.
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
+}
+
+// readIendtifier reads an identifier.
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// isLetter returns true if the character is a letter.
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 // NextToken returns the next token.
 func (l *Lexer) NextToken() token.Token {
-    var tok token.Token
+	var tok token.Token
 
-    switch l.ch {
+	switch l.ch {
 	case ':':
-		l.readChar()
-		if l.input[l.position] == '=' {
-			tok = newToken(token.EQULON, ':')
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = newToken(token.DECLARE, ':')
 		} else {
 			tok = newToken(token.ILLEGAL, ':')
 		}
-    case '=':
-        tok = newToken(token.ASSIGN, l.ch)
-    case ';':
-        tok = newToken(token.SEMICOLON, l.ch)
-    case '(':
-        tok = newToken(token.LPAREN, l.ch)
-    case ')':
-        tok = newToken(token.RPAREN, l.ch)
-    case ',':
-        tok = newToken(token.COMMA, l.ch)
-    case '+':
-        tok = newToken(token.PLUS, l.ch)
-    case '{':
-        tok = newToken(token.LBRACE, l.ch)
-    case '}':
-        tok = newToken(token.RBRACE, l.ch)
-    case 0:
-        tok.Literal = ""
-        tok.Type = token.EOF
-    }
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
 
-    l.readChar()
-    return tok
+	l.readChar()
+	return tok
 }
 
 // newToken is a helper function to create a new token.
 func newToken(tokenType token.TokenType, ch byte) token.Token {
-	if ch == ':' {
+	if tokenType == token.DECLARE {
 		return token.Token{Type: tokenType, Literal: ":="}
 	}
-    return token.Token{Type: tokenType, Literal: string(ch)}
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
